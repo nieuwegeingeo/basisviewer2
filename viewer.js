626ff2f75d246ca33d8b3c90f31142d043978673
Michiel Kselik <m.kselik@nieuwegein.nl>
2020-06-23 14:43:32 +0200

    Includes functie vervangen door indexOf in foto splitter

--- viewer.js
+++ viewer.js
@@ -268,7 +268,7 @@ Geogem.formatAttributes = function (attributes, title, fields) {
 				//value = "http://geoserver.nieuwegein.nl/beheer2014fotos/m_reparatieplekken_reparatieplek8.jpg";
 				if (value && value.length > 5) {
 						html += '<tr class="inforow' + rowstyle + '"><td class="first">' + fields[field] + '</td></tr>';
-					if (value.includes('|')) {
+					if (value.indexOf('|') !== -1) {
 						html += '<tr><td colspan="2">';
 						var fotoarray = value.split('|');
 						$.each($(fotoarray), function(index, item) {
