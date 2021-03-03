6e24e46a89798232881f4704b17c675641421134
Michiel Kselik <m.kselik@nieuwegein.nl>
2021-03-03 09:55:26 +0100

    Cleanup

--- extra_functies/alt_layerswitcher.js
+++ extra_functies/alt_layerswitcher.js
@@ -7,17 +7,19 @@ function loadAltLayerswitcher() {
                      '<div class="layer-switcher__content"></div>')
 }
 
-showHideLayerSwitcher = function() {
-            if ($('.layer-switcher__container').hasClass('zichtbaar-flex')) {
-                    $('.layer-switcher__container').removeClass('zichtbaar-flex');
-                    $('.layer-switcher__button').addClass('zichtbaar-block');
-                } else {
-                    $('.layer-switcher__container').addClass('zichtbaar-flex');
-                    $('.layer-switcher__button').removeClass('zichtbaar-block');
-                }
+    showHideLayerSwitcher = function() {
+        if ($('.layer-switcher__container').hasClass('zichtbaar-flex')) {
+                $('.layer-switcher__container').removeClass('zichtbaar-flex');
+                $('.layer-switcher__button').addClass('zichtbaar-block');
+        } else {
+            $('.layer-switcher__container').addClass('zichtbaar-flex');
+            $('.layer-switcher__button').removeClass('zichtbaar-block');
         }
+    }
+    
     $('.layer-switcher__view').on('click', showHideLayerSwitcher)
     addDomElements();
+    
     $.each(Geogem.map.layers, function(){
         if ($(this).prop('displayInLayerSwitcher') === true) {
         layerVisibility = '';
@@ -25,22 +27,45 @@ showHideLayerSwitcher = function() {
                 layerVisibility = 'checked=""'
             }
             layerName = $(this).prop('name');
-            $('.layer-switcher__content').append('<table class="layer-switcher__list"><tr class="layer-switcher__item no-select" ><td><label class="layer-switcher__label"><input class="layer-switcher__input" type="checkbox" '+ layerVisibility +' name="'+ layerName +'">'+ layerName +'</label></td></tr></table>');
+            if (Geogem.Settings.altLayerswitcherRadio === true) {  
+                $('.layer-switcher__content').append('<table class="layer-switcher__list"><tr class="layer-switcher__item no-select" ><td><label class="layer-switcher__label"><input class="layer-switcher__input" type="radio" '+ layerVisibility +' value="'+ layerName +'" name="button-group">'+ layerName +'</label></td></tr></table>');
+            } else {
+                $('.layer-switcher__content').append('<table class="layer-switcher__list"><tr class="layer-switcher__item no-select" ><td><label class="layer-switcher__label"><input class="layer-switcher__input" type="checkbox" '+ layerVisibility +' value="'+ layerName +'" name="button-group">'+ layerName +'</label></td></tr></table>');
+            }
         }
     });
-
+    
+    function setRadioVis() {
+        if (Geogem.Settings.altLayerswitcherRadio === true) {  
+            $.each($('.layer-switcher__input'),function(i,e) {  
+                var thisLayer = Geogem.map.getLayersByName(e.value)[0];   
+                if (e.checked === true){
+                    thisLayer.setVisibility(true);
+                } else {
+                    thisLayer.setVisibility(false);
+                }
+            })
+        } 
+    }
+    
+    setRadioVis();
+    
     Geogem.map.layerInput = $('.layer-switcher__item').children().children('label').children('input');
 
-    $(Geogem.map.layerInput).change( function(){
+    $(Geogem.map.layerInput).change( function() {
+        var clickedInput = $(this);
         if ($(this).prop('checked') === true) {
-            mapLayer = Geogem.map.getLayersByName($(this).prop('name'))[0];
+            var mapLayer = Geogem.map.getLayersByName(clickedInput.prop('value'))[0];
             mapLayer.setVisibility(true);
+            
+            setRadioVis();
+            
         } else {
-            mapLayer = Geogem.map.getLayersByName($(this).prop('name'))[0];
+            var mapLayer = Geogem.map.getLayersByName(clickedInput.prop('value'))[0];
             mapLayer.setVisibility(false);
         }
     })
-
+    
     $('.layer-switcher__view').on('click', showHideLayerSwitcher);
 }
 
